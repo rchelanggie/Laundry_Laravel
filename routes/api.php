6361f3b68851c087f11cb4f6b2676340bd416a9c
rchelanggie <rachelpasorong@gmail.com>
2023-08-07 21:26:17 +0700

    Commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\api\LaundryController;
+use App\Http\Controllers\api\PromoController;
+use App\Http\Controllers\api\ShopController;
+use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/promo',[PromoController::class, 'readAll']);
+Route::get('/shop',[ShopController::class, 'readAll']);
+Route::get('/laundry',[LaundryController::class, 'readAll']);
+Route::get('/user',[UserController::class, 'readAll']);
+
+Route::post('/register',[UserController::class, 'register']);
+Route::post('/login',[UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    // Laundry
+    Route::get('/laundry/user/{id}',[LaundryController::class, 'whereUserId']);
+    Route::post('/laundry/claim',[LaundryController::class, 'claim']);
+
+    // Promo
+    Route::get('/promo/limit',[PromoController::class, 'readLimit']);
+
+    // Shop
+    Route::get('/shop/recommendation/limit',[ShopController::class, 'readRecommendationLimit']);
+    Route::get('/shop/search/city/{name}',[ShopController::class, 'searchByCity']);
 });
